d949e7a478db4f9bdcb0cb297ced779e9c1170ca
PENG JINGQIANG <91521556+YannouPio@users.noreply.github.com>
2025-02-23 16:17:29 +0800

    Add authentication context and update Header component for user state management

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,21 +1,31 @@
+"use client"
+
 import React from "react";
-import Img from "next/image";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Authentication from "./Authentication";
+import { useAuthContext } from "../provider";
 
 function Header() {
+  const {user} = useAuthContext();
   return (
     <div>
       <div className="p-5 flex items-center justify-between">
         <div className="flex items-center gap-3">
-          <Img src="/logo.svg" alt="logo" width={40} height={40} />
+          <Image src="/logo.svg" alt="logo" width={40} height={40} />
           <h2 className="text-2xl font-bold">视频生成器</h2>
         </div>
 
         <div>
-          <Authentication>
+          {!user ? <Authentication>
             <Button>Get Started</Button>
-          </Authentication>
+          </Authentication> :
+            <div className="flex items-center gap-3">
+              <Link href={'/dashboard'}></Link>
+              <Button>仪表盘</Button>
+              <Image src={user?.photoURL} alt="user" width={40} height={40} className="rounded-full" />
+            </div>
+          }
         </div>
       </div>
     </div>
--- app/provider.js
+++ app/provider.js
@@ -1,10 +1,29 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/configs/firebaseConfig";
+import { AuthContext } from "./_context/AuthContext";
+
+
 function Provider({ children }) {
-  return (
-    <div>
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+              console.log(user);
+              setUser(user);
+            } else {
+                console.log("No user");
+            }
+        });
+        return () => unsubscribe();
+    }, []);
+    return (
+      <div>
+        <AuthContext.Provider value={{ user }}>
       <NextThemesProvider
         attribute="class"
         defaultTheme="dark"
@@ -12,9 +31,16 @@ function Provider({ children }) {
         disableTransitionOnChange
       >
         {children}
-      </NextThemesProvider>
+          </NextThemesProvider>
+          </AuthContext.Provider>
     </div>
   );
+}
+
+
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  return context;
 }
 
 export default Provider;
